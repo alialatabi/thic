05782592bc6ac8fedf1471d20375467400e7611f
alialatabi <ali.alatabi99.vb@gmail.com>
2022-10-17 14:06:04 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin: {
+        "-13rem": "-13rem",
+        "-16rem": "-16rem",
+        "-17rem": "-17rem",
+      },
+    },
+  },
+  plugins: [],
+};
