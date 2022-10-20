c40ab043c346f31e42df79b2161f154a59a080ce
alialatabi <ali.alatabi99.vb@gmail.com>
2022-10-20 12:02:15 +0300

    added the about componant

---
+++ src/Componants/About.js
@@ -0,0 +1,42 @@
+import infinity from "../static/images/Infinity.png";
+import dots from "../static/images/dots.png";
+import iso from "../static/images/iso.png";
+
+export default function Landing() {
+  return (
+    <div className="flex flex-col bg-[#F1F2F4] mt-2 px-6 mx-auto space-y-6">
+      <div className="flex flex-row">
+        <p className="text-2xl text-slate-800 font-semibold mt-8">About Us</p>
+        <img
+          className="absolute right-0 w-36"
+          src={infinity}
+          alt="infinit insurance"
+        />
+      </div>
+
+      <div className="flex flex-row items-center justify-center ml-16 z-10">
+        <img className="w-36" src={dots} alt="infinit insurance" />
+        <p className="text-md text-slate-500 font-semibold ml-4 w-[38%]">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, sum
+        </p>
+      </div>
+
+      <div className="flex flex-row items-center justify-center ml-16 relative z-0 ">
+        <img className="w-[35%] -mt-14" src={iso} alt="infinit insurance" />
+        <p className="text-md text-slate-500 font-semibold ml-16 pr-16 w-[55%]">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/Componants/Landing.js
@@ -0,0 +1,45 @@
+import blgs from "../static/images/blgs.png";
+import blgsbg from "../static/images/blgsbg.png";
+export default function Landing() {
+  return (
+    <div className=" container mt-36 flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
+      <div className="lg:w-[50%] landing-animate-text">
+        <div className="lg:max-w-lg">
+          <h1 className="text-2xl font-bold tracking-wide text-gray-800 lg:text-4xl -mt-[40%]">
+            Trust House Insurance
+          </h1>
+
+          <div className="mt-8 space-y-5">
+            <p className="flex items-center -mx-2 text-slate-500 font-semibold">
+              <span className="mx-2">
+                Dar Al Theqa Insurance Company (Trust House Insurance company) a
+                private shareholding was founded by Iraqi insurance men whom
+                worked long years outside Iraq, Trust Group and businessmen in
+                Iraq. The company was registered in Baghdad on 30/09/2009 with a
+                fully paid share capital of IQD 1,012,000,000 increased in March
+                2014 to IQD 5,000,000,000 in compliance with the Insurance Diwan
+                Order concerning insurance companies’ capital.
+              </span>
+            </p>
+            <button className="text-white bg-gray-700 hover:bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center md:mr-0 ">
+              See More
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center w-full relative h-[100%] lg:w-[75%] lg:h-[150%] landing-animate-image">
+        <img
+          className="w-full h-auto mx-auto lg:max-w-2xl absolute z-0"
+          src={blgsbg}
+          alt="Fire Insurance"
+        />
+        <img
+          className="w-full h-full mx-auto lg:max-w-2xl absolute z-10"
+          src={blgs}
+          alt="Trust House Insurance Company"
+        />
+      </div>
+    </div>
+  );
+}
